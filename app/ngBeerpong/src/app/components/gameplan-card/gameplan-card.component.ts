94b9eb750203789c2765ddbb9db778a9acd2647f
GlaDom <dominik8.glaser@web.de>
2024-06-14 12:35:45 +0200

    make winner and looser visible in gameplan

--- app/ngBeerpong/src/app/components/gameplan-card/gameplan-card.component.ts
+++ app/ngBeerpong/src/app/components/gameplan-card/gameplan-card.component.ts
@@ -21,4 +21,25 @@ export class GameplanCardComponent {
 
   constructor() {}
 
+  getColor(points: number | undefined): string {
+    let retval: string = ""
+    if(points && this.match) {
+      if(points == 0) {
+        return retval
+      } else if(points == this.match.points_home) {
+        if(points>this.match.points_away) {
+          retval = "bg-green-400"
+        } else {
+          retval = "bg-red-400"
+        }
+      } else {
+        if(points>this.match.points_home) {
+          retval = "bg-green-400"
+        } else {
+          retval = "bg-red-400"
+        }
+      }
+    }
+    return retval
+  }
 }
