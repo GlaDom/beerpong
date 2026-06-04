09f50d1239bdd21ea1082c11037ffc8454b4079f
GlaDom <dominik8.glaser@web.de>
2026-06-04 10:44:35 +0200

    update prod env

--- app/ngBeerpong/src/environments/environment.production.ts
+++ app/ngBeerpong/src/environments/environment.production.ts
@@ -1,12 +1,12 @@
 import { IEnvironment } from "./ienvironment";
 
 export const environment: IEnvironment = {
-    production: false,
+    production: true,
     auth: {
         domain: 'dev-nduro5lf8x5ddjgj.eu.auth0.com',
-        clientId: 'f5We2HLhj4JInznJZHZYY6eXDz6I3AEz',
+        clientId: 'VdAWn9Kh3BSCS0VgQH8JOI700ol0hCoH',
         audience: 'https://dev-nduro5lf8x5ddjgj.eu.auth0.com/api/v2/',
-        redirectUri: 'https://skbeerpong.com:4200/home,https://skbeerpong.com:4200/callback'
+        redirectUri: 'https://skbeerpong.com'
     },
     api: {
         url: 'https://localhost:8082/api'
